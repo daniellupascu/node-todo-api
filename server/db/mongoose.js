ad45cb08d732ebb9e89599e214415785c5c7a3e4
Daniel Lupascu <dl@hoej.dk>
2018-11-21 13:03:19 +0100

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds019143.mlab.com:19143/node-api', { useNewUrlParser: true });
+// mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds019143.mlab.com:19143/node-api', { useNewUrlParser: true });
+mongoose.connect('mongodb://daniel:123123qwe@ds263367.mlab.com:19143/node-api', { useNewUrlParser: true });
+
 
 module.exports = {mongoose};
