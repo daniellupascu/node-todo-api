77d9bc097d588ac36fb7262b303a5d439f6f4ad2
Daniel Lupascu <dl@hoej.dk>
2018-11-24 23:29:37 +0100

    add test for DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -384,3 +384,21 @@ describe('POST /users/login', () => {
     });
 
 });
+
+describe('DELETE /users/me/token', () => {
+    it('shoudl remove auth token on logout', done => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', seededUsers[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+                User.findById(seededUsers[0]._id).then(user => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch(e => done(e));
+            });
+    })
+});
