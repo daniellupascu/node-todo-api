34c8a63987d09bdaecb5d9bb1b62d5693e472fbe
Daniel Lupascu <dl@hoej.dk>
2018-11-26 23:00:35 +0100

    rollbacl config

--- server/config/config.js
+++ server/config/config.js
@@ -1,19 +1,19 @@
 var env = process.env.NODE_ENV || 'development';
-var config = require('./config.json');
+// var config = require('./config.json');
 
-if(env === 'development' || env === 'test') {
-    var envConfig = config[env];
-    Object.keys(envConfig).forEach(key => {
-        process.env[key] = envConfig[key]
-    });
-}
-
-// if (env === 'development') {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
-// } else if (env === 'test') {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-// } else if (env === 'production') {
-//     process.env.MONGODB_URI = 'mongodb://daniel:123123qwe@ds113454.mlab.com:13454/node-api';
+// if(env === 'development' || env === 'test') {
+//     var envConfig = config[env];
+//     Object.keys(envConfig).forEach(key => {
+//         process.env[key] = envConfig[key]
+//     });
 // }
+
+if (env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
+} else if (env === 'production') {
+    process.env.MONGODB_URI = 'mongodb://daniel:123123qwe@ds113454.mlab.com:13454/node-api';
+}
